f2b5007c5bd28b791b9dc1eb592d520310a2c668
Eli Vallerino <131781289+elivallerino@users.noreply.github.com>
2023-06-08 10:12:14 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,21 @@
+function texto()
+{
+	var t=document.getElementById("textoo");
+	t.style.color="blue";
+	t.style.backgroundColor="#006699";
+	var fondo=document.getElementById("fondo");
+	fondo.style.backgroundImage="url('imagenes/1.jpg')";
+	t.innerHTML="Holaaaa";
+}
+function texto2()
+{
+	
+	var t=document.getElementById("textoo");
+	t.style.color="white";
+	t.style.backgroundColor="white";
+	var fondo=document.getElementById("fondo");
+	fondo.style.backgroundImage="url('imagenes/estrellas.jpg')";
+	t.innerHTML="- Los Cuatro Elementos de la Astrología -";
+}
+
+
